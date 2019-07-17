a776c40cf3814a93c45781933962f420ad6df1b3
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-07-16 23:51:17 -0400

    Got enemies and bullets

--- main.cpp
+++ main.cpp
@@ -3,6 +3,43 @@
 using namespace shapegame;
 
 
+class Enemy : public Rectangle {
+    public:
+        Enemy(Position pos) : Rectangle(40, 40, pos, Color::RED) {}
+        void update() override {
+            this->translate(0, 10);
+
+            /* If this enemy is below the screen */
+            if (this->pos.y > 700) {
+                /* This kill its self */
+                this->kill();
+            }
+        }
+};
+
+
+class Bullet : public Rectangle {
+    public:
+        /* A constructor for our bullet class that takes a Position as an argument
+         * It then initilizes the rectangle that will be representing the bullet
+         * with a width, height, position, and color */
+        Bullet(Position pos) : Rectangle(5, 30, pos, Color::WHITE) {}
+
+
+        /* Override the update method. This will be called every frame by the
+         * engine once the object instance is added to the scene */
+        void update() override {
+            /* Every frame move up pixels */
+            this->translate(0, -30);
+
+            /* If this bullet is above the screen */
+            if (this->pos.y < -30) {
+                /* This kill its self */
+                this->kill();
+            }
+        }
+};
+
 /* Create a player class which extends
  * TriangleIsosceles built in class. In order to hook into the callbacks
  * of the engine you need to create a class that overrides the callback
@@ -13,7 +50,7 @@ class Player : public TriangleIsosceles {
         /* Construct the player with a triangle 100px wide, 100px tall
          * at position 100, 100, and with a color of green
          */
-        Player() : TriangleIsosceles(100, 100, Position(100, 100), Color::GREEN) {}
+        Player() : TriangleIsosceles(50, 50, Position(100, 100), Color::GREEN) {}
 
         /* Override the update method. This method is called every frame */
         void update() override {
@@ -21,7 +58,31 @@ class Player : public TriangleIsosceles {
             this->setPosition(Position(Input::Mouse::getPos().x, 590));
             /* Move the player over 50 pixels so the mouse is in the middle */
             /* Origin is the corner by default */
-            this->translate(-50, 0);
+            this->translate(-25, 0);
+        }
+        /* override the mouse click handler to recieve callbacks when
+         * mouse clicks happen
+         */
+        void onMouseClick(Input::Mouse::Btn btn, Input::Action action) override {
+            /* check to see if the left mouse button is down */
+            if (btn == Input::Mouse::Btn::LEFT && action == Input::Action::DOWN) {
+                /* if it is down then shoot */
+
+                /* A lot of things are happening here. Were creating an instance of
+                 * bullet using make_unique
+                 * Were adding that bullet to the scene.
+                 * Notice that we are getting the scene by accessing the game
+                 * instance via the inst() static method of the Game object.
+                 * Then notice that scene->addChild returns a reference to the
+                 * child that was added. We can use this reference to perform
+                 * operations on the object even though it is already added to the
+                 * scene. This is why we can move it around via the translate
+                 * method call below*/
+                Object *bullet = Game::inst().scene->addChild(std::make_unique<Bullet>(this->pos));
+                /* Move the bullet so it looks like its coming out of the tip of
+                 * the player */
+                bullet->translate(25, -40);
+            }
         }
 };
 
@@ -37,6 +98,7 @@ int main() {
     /* Add a child to the scene */
 	//game.scene->addChild(new TriangleIsosceles(100, 100, Position(100, 100), Color::BLACK));
 	game.scene->addChild(std::make_unique<Player>());
+	game.scene->addChild(std::make_unique<Enemy>(Position(100, 0)));
 
     game.run();
 }
